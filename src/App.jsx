6ac5c674d1d6e57fb18f08481c7e8545111a1a63
Moussa <juns@MacBookPro.lan>
2026-06-02 23:52:39 +0200

    Revert "allow multiple slots same day"
    This reverts commit c70b81622e4ea01c663b6fb3f155cbc477f25562.

--- src/App.jsx
+++ src/App.jsx
@@ -947,6 +947,7 @@ function CalendarPicker({ selected, onChange }) {
     if (exists) {
       onChange(selected.filter(s => s.key !== key));
     } else {
+      const filtered = selected.filter(s => s.date !== date);
       onChange([...filtered, { key, date, hour, label: date + " a " + hour }]);
     }
   }
