22b5f3ee297ff41c4f6c0f9e87c34c65d48cc65b
Votre Nom <vous@exemple.com>
2026-06-14 12:53:58 +0200

    force new bundle hash v2

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/[name]-[hash]-v2.js`,
+        chunkFileNames: `assets/[name]-[hash]-v2.js`,
+      }
+    }
+  }
 })
